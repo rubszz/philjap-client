b3939b9ca4f3e3d5c768270dbb014a65af96f4f8
rubszz <rubs76593@gmail.com>
2023-06-06 11:29:20 +0800

    Added Navbar

--- client/src/pages/RegisterEngineer.jsx
+++ client/src/pages/RegisterEngineer.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import RegisterForm from '../components/RegisterForm'
+import Navbar from '../components/Navbar'
 
 const RegisterEngineer = () => {
   return (
     <div>
         <div>
-            <nav className="flex flex-col gap-4 justify-between p-4 text-white">
-                <a className="font-bold p-2 hover:text-violet-500" href="/dashboard-admin">Home</a>
-            </nav>
+           <Navbar />
         </div>
-        <div className="">
+        <div className="pt-20">
             <RegisterForm />
         </div>
     </div>
