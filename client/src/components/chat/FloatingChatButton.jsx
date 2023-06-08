ad0b8d671e1826decddcd0b34e467c17f608fd0e
rubszz <rubs76593@gmail.com>
2023-06-08 02:23:56 -0400

    Email Support Update

--- client/src/components/chat/FloatingChatButton.jsx
+++ client/src/components/chat/FloatingChatButton.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 const FloatingChatButton = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
+  const form = useRef();
 
   const handleSubmit = (e) => {
+
+
     e.preventDefault();
     // Handle form submission logic here, e.g., send data to server
     console.log('Email:', email);
     console.log('Name:', name);
     console.log('Message:', message);
+    emailjs.sendForm('service_zjcigin', 'template_chnknaq', form.current, '4b35hCQFivBvVPHWY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+    });
+    alert("Message Sent! Please wait for the email support team to contact you thru your email. Thank you!");
     // Reset form fields
     setEmail('');
     setName('');
@@ -26,17 +38,18 @@ const FloatingChatButton = () => {
           className="bg-discordBlue text-white p-3 rounded-full shadow-lg"
           onClick={() => setIsOpen(true)}
         >
-          Chat Support
+          Email Support
         </button>
       )}
       {isOpen && (
         <div className="bg-discordDark p-4 rounded-lg shadow-lg max-w-sm">
-          <h3 className="text-white text-lg font-semibold mb-2">Chat Support</h3>
-          <form onSubmit={handleSubmit}>
+          <h3 className="text-white text-lg font-semibold mb-2">Email Support</h3>
+          <form ref={form} onSubmit={handleSubmit}>
             <div className="mb-4">
               <label className="block text-white text-sm mb-1">Email</label>
               <input
                 type="email"
+                name="user_email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 className="w-full bg-discordInput focus:outline-none text-white py-2 px-3 rounded"
@@ -48,6 +61,7 @@ const FloatingChatButton = () => {
               <label className="block text-white text-sm mb-1">Name</label>
               <input
                 type="text"
+                name="user_name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 className="w-full bg-discordInput focus:outline-none text-white py-2 px-3 rounded"
@@ -59,6 +73,7 @@ const FloatingChatButton = () => {
               <label className="block text-white text-sm mb-1">Message</label>
               <textarea
                 value={message}
+                name="message"
                 onChange={(e) => setMessage(e.target.value)}
                 className="w-full bg-discordInput focus:outline-none text-white py-2 px-3 rounded h-20 resize-none"
                 placeholder="Enter your message"
