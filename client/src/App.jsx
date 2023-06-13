82fe011f67a7571d4c71ac9082546214371127cc
rubszz <rubs76593@gmail.com>
2023-06-13 09:14:25 +0800

    Operations Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LoginPage from './pages/LoginPage'
 import FloatingChatButton from './components/chat/FloatingChatButton'
+import AboutUs from './pages/AboutUs'
 
 
 function App() {
 
   return (
-    <div className="bg-slate-900 flex flex-row justify-between">
+    <div className="bg-slate-900 flex flex-col justify-between">
+      <AboutUs />
       <FloatingChatButton />
       <LoginPage />
     </div>
---
+++ client/src/pages/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div className="text-white pl-5">
+        <div className="flex flex-row">
+            <h1 className="font-poppins font-semibold text-2xl">About Philjap <br/>Company</h1>
+        </div>
+    </div>
+  )
+}
+
+export default AboutUs
